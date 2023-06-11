e5116be55005688cc450f55556e0819b0a7f2a56
can <can@pop-os.localdomain>
2023-06-11 19:24:10 +0300

    Update gitignore, Read last check point to continou learning in sb3cadet

--- src/sb3cadet.py
+++ src/sb3cadet.py
@@ -1,3 +1,5 @@
+import os
+import glob
 import yaml
 from stable_baselines3 import A2C
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
@@ -25,6 +27,8 @@ parser.add_argument('--agentRed', metavar='agentRed', type=str,
                     help='Class name of Red Agent')
 parser.add_argument('--numOfMatch', metavar='numOfMatch', type=int, nargs='?', default=10,
                     help='Number of matches to play between agents')
+parser.add_argument('--keepLastModel', action='store_true',
+                    help='Keep last model and use it in the game')
 parser.add_argument('--render', action='store_true',
                     help='Render the game')
 parser.add_argument('--gif', action='store_true',
@@ -63,6 +67,12 @@ class LoggerCallback(BaseCallback):
         return True
 
 
+def get_latest_check_point_path():
+    check_dir = 'models/RiskyValley_Default'
+    list_of_files = glob.glob(check_dir+'/*')
+    latest_check_point_path = max(list_of_files, key=os.path.getctime)
+    return latest_check_point_path
+    
 if __name__ == "__main__":
 
     hyperparams = read_hypers()
@@ -76,8 +86,22 @@ if __name__ == "__main__":
         )
 
         env = SubprocVecEnv([lambda: OwnRiskyValley(args, agents) for i in range(hyperparam["env"]["n_envs"])])
-        checkpoint_callback = CheckpointCallback(save_freq=1000, save_path='./models/RiskyValley_Default',
+        checkpoint_callback = CheckpointCallback(save_freq=100000, save_path='./models/RiskyValley_Default',
                                                  name_prefix='tsts')
+        if not args.keepLastModel:
+            model = A2C(env=env,
+                        verbose=1,
+                        tensorboard_log="logs",
+                        **hyperparam["agent"])
+        else:
+            a2c_path = get_latest_check_point_path()
+            print(a2c_path)
+            model = A2C.load(
+                path=a2c_path,
+                env=env,
+                tensorboard_log="logs",
+                **hyperparam["agent"]
+            )
 
         model = A2C(env=env,
                     verbose=1,
